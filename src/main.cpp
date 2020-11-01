cbcc22bafc74d92fd07a2b5aace2b73f4b438f4c
Josh Bowman <jbowman353@gmail.com>
2020-10-31 22:06:52 -0400

    Implement -h and -l flag functionality

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,12 @@ namespace po = boost::program_options;
 
 const std::string BAD_ROLL_MSG = "Please specify number and type of dice. Format: #d#";
 
+bool dieSort(diceutil::Die d1, diceutil::Die d2) { return d1.getVal() < d2.getVal(); }
+
 void parseRoll(std::string rollStr, unsigned int keepHighest, unsigned int keepLowest) {
     std::regex dieRollRegex("[0-9]+d[0-9]+");
     if (std::regex_match(rollStr, dieRollRegex)) {
-        // Parse the roll
+        // Parse the roll string
         auto dLoc = rollStr.find('d');
         uint nDice = std::stoi( rollStr.substr(0, dLoc) );
         uint nSides = std::stoi( rollStr.substr(dLoc + 1) );
@@ -32,8 +34,46 @@ void parseRoll(std::string rollStr, unsigned int keepHighest, unsigned int keepL
             diceutil::Die d{nSides};
             d.roll();
             dice.push_back(d);
-            std::cout << d.getVal() << "  ";
         }
+
+        std::sort(dice.begin(), dice.end(), dieSort);
+        int total = 0;
+        int numElements = dice.size();
+
+        if (keepHighest) {
+            // only print highest n
+            std::cout << "KEPT: ";
+            for (int i = numElements - 1; i > numElements - 1 - keepHighest; i--) {
+                std::cout << dice[i].getVal() << "  ";
+                total += dice[i].getVal();
+            }
+            std::cout << std::endl;
+
+            std::cout << "DISCARDED: ";
+            for (int j = numElements - 1 - keepHighest; j >= 0; j--) {
+                std::cout << dice[j].getVal() << "  ";
+            }
+        } else if (keepLowest) {
+            // only print lowest n
+            std::cout << "KEPT: ";
+            for (int i = 0; i < keepLowest; i++) {
+                std::cout << dice[i].getVal() << "  ";
+                total += dice[i].getVal();
+            }
+            std::cout << std::endl;
+
+            std::cout << "DISCARDED: ";
+            for (int j = keepLowest; j < numElements; j++) {
+                std::cout << dice[j].getVal() << "  ";
+            }
+        } else {
+            // print 'em all
+            for (diceutil::Die d: dice) {
+                std::cout << d.getVal() << "  ";
+                total += d.getVal();
+            }
+        }
+        std::cout << std::endl << std::endl << "TOTAL: " << total << std::endl;
         std::cout << std::endl << std::endl;
 
     } else {
